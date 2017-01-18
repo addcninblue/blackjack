8f66cc496f43d55d94ebd57211e8aed341a25a4b
Darian Nguyen <dariann6@gmail.com>
2017-01-17 23:16:40 -0800

    Started working on GUI

---
+++ src/gui/GamePanel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Darian
+ */
+public class GamePanel extends JPanel {
+    public GamePanel() {
+        
+    }
+}
